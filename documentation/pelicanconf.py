f2dfbd6f4d8f4f32d9c3dd2913af3b7b9ded7075
david-figuer <davidfiguer@usal.es>
2026-09-18 09:23:07 +0200

    Modify Elena Calle Clemente's blurb format
    Updated the blurb for Elena Calle Clemente with HTML tags.

--- documentation/pelicanconf.py
+++ documentation/pelicanconf.py
@@ -281,7 +281,7 @@ AUTHORS = {
     #"Elena Calle Clemente": {
     #    "url": "",
     #    "role": "Colaborador",
-    #    "blurb": "**Elena Calle Clemente** (Elenasa) es astrónoma y estudiante de Física en la Universidad de Salamanca. Vinculada a la ciencia desde pequeña, lleva más de cinco años volcada en proyectos de divulgación científica, ejerce profesionalmente como astrónoma con Astroiberia, ademas es secretaria de la asociación universitaria *Supernova* y lidera como pionera el proyecto de accesibilidad *Clarity Blind* junto a la ONCE. Apasionada de las coincidencias matemáticas y de explicar el cosmos de forma cercana y apasionante.",
+    #    "blurb": "<strong>Elena Calle Clemente</strong> (Elenasa) es astrónoma y estudiante de Física en la Universidad de Salamanca. Vinculada a la ciencia desde pequeña, lleva más de cinco años volcada en proyectos de divulgación científica, ejerce profesionalmente como astrónoma con Astroiberia, ademas es secretaria de la asociación universitaria <strong>Supernova</strong> y lidera como pionera el proyecto de accesibilidad <strong>Clarity Blind</strong> junto a la ONCE. Apasionada de las coincidencias matemáticas y de explicar el cosmos de forma cercana y apasionante.",
     #    "avatar": "images/elena.jpeg",
     #    "articles_url": "autor/elena-calle-clemente",       
     #},
